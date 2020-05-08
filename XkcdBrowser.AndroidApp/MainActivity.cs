8a136375d11605a160d6c3283784692daf5886a4
John Hunter <hunterjw.usa@gmail.com>
2020-05-08 18:12:51 -0500

    Initial android app prototype

---
+++ XkcdBrowser.AndroidApp/MainActivity.cs
@@ -0,0 +1,89 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using ImageViews.Photo;
+using Android.Content;
+using System;
+using XkcdBrowser.Models;
+
+namespace XkcdBrowser.AndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Comic CurrentComic { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+            Button firstButton = FindViewById<Button>(Resource.Id.firstButton);
+            Button previousButton = FindViewById<Button>(Resource.Id.previousButton);
+            Button randomButton = FindViewById<Button>(Resource.Id.randomButton);
+            Button nextButton = FindViewById<Button>(Resource.Id.nextButton);
+            Button latestButton = FindViewById<Button>(Resource.Id.latestButton);
+
+            firstButton.Click += FirstButton_Click;
+            previousButton.Click += PreviousButton_Click;
+            randomButton.Click += RandomButton_Click;
+            nextButton.Click += NextButton_Click;
+            latestButton.Click += LatestButton_Click;
+
+            CurrentComic = XkcdAndroid.LatestComic(ApplicationContext, comicTitleView, comicPhotoView);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void FirstButton_Click(object sender, EventArgs args)
+        {
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+
+            CurrentComic = XkcdAndroid.FirstComic(ApplicationContext, comicTitleView, comicPhotoView);
+        }
+
+        public void PreviousButton_Click(object sender, EventArgs args)
+        {
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+
+            CurrentComic = XkcdAndroid.PreviousComic(ApplicationContext, comicTitleView, comicPhotoView, CurrentComic);
+        }
+
+        public void RandomButton_Click(object sender, EventArgs args)
+        {
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+
+            CurrentComic = XkcdAndroid.RandomComic(ApplicationContext, comicTitleView, comicPhotoView);
+        }
+
+        public void NextButton_Click(object sender, EventArgs args)
+        {
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+
+            CurrentComic = XkcdAndroid.NextComic(ApplicationContext, comicTitleView, comicPhotoView, CurrentComic);
+        }
+
+        public void LatestButton_Click(object sender, EventArgs args)
+        {
+            TextView comicTitleView = FindViewById<TextView>(Resource.Id.comicTextView);
+            PhotoView comicPhotoView = FindViewById<PhotoView>(Resource.Id.comicPhotoView);
+
+            CurrentComic = XkcdAndroid.LatestComic(ApplicationContext, comicTitleView, comicPhotoView);
+        }
+    }
+}
---
+++ XkcdBrowser.AndroidApp/XkcdAndroid.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Android.Content;
+using Android.Graphics;
+using Android.Widget;
+using XkcdBrowser.Models;
+
+namespace XkcdBrowser.AndroidApp
+{
+	public static class XkcdAndroid
+	{
+		private static void SetupDatabase(Context context, out string dataFolder)
+		{
+			dataFolder = System.IO.Path.Combine(context.GetExternalFilesDir("").ToString(), "XkcdData");
+			if (!Directory.Exists(dataFolder))
+			{
+				Directory.CreateDirectory(dataFolder);
+			}
+
+			XkcdDatabase.DatabaseLocation = System.IO.Path.Combine(dataFolder, "xkcd.db");
+		}
+
+		private static void DownloadComic(Comic comic, string dataFolder, out string comicPath)
+		{
+			comicPath = System.IO.Path.Combine(dataFolder, System.IO.Path.GetFileName(comic.ImageUrl));
+
+			if (!File.Exists(comicPath))
+			{
+				using WebClient client = new WebClient();
+				client.DownloadFile(new Uri(comic.ImageUrl), comicPath);
+			}
+		}
+
+		private static void RefreshDatabase(Context context)
+		{
+			// todo: make this smarter so we aren't always refreshing the database
+			SetupDatabase(context, out _);
+			Xkcd.RefreshComicDictionary();
+		}
+
+		public static void LoadComic(Context context, TextView comicTitleView, ImageView comicImageView, Comic comic)
+		{
+			// Clear out displays
+			comicTitleView.Text = string.Empty;
+			comicImageView.SetImageDrawable(null);
+
+			try
+			{
+				SetupDatabase(context, out string dataFolder);
+
+				DownloadComic(comic, dataFolder, out string comicPath);
+
+				comicTitleView.Text = comic.Title;
+				Bitmap bitmap = BitmapFactory.DecodeFile(comicPath);
+				comicImageView.SetImageBitmap(bitmap);
+			}
+			catch
+			{
+				comicTitleView.Text = "Error!";
+			}
+		}
+
+		public static Comic FirstComic(Context context, TextView comicTitleView, ImageView comicImageView)
+		{
+			SetupDatabase(context, out _);
+			Comic first = Xkcd.GetFirstComic();
+
+			LoadComic(context, comicTitleView, comicImageView, first);
+
+			return first;
+		}
+
+		public static Comic PreviousComic(Context context, TextView comicTitleView, ImageView comicImageView, Comic current)
+		{
+			SetupDatabase(context, out _);
+			Comic previous = current.Previous();
+			if (previous != null)
+			{
+				LoadComic(context, comicTitleView, comicImageView, previous);
+				return previous;
+			}
+			return current;
+		}
+
+		public static Comic RandomComic(Context context, TextView comicTitleView, ImageView comicImageView)
+		{
+			SetupDatabase(context, out _);
+			Comic random = Xkcd.GetRandomComic();
+
+			LoadComic(context, comicTitleView, comicImageView, random);
+
+			return random;
+		}
+
+		public static Comic NextComic(Context context, TextView comicTitleView, ImageView comicImageView, Comic current)
+		{
+			SetupDatabase(context, out _);
+			Comic next = current.Next();
+			if (next != null)
+			{
+				LoadComic(context, comicTitleView, comicImageView, next);
+				return next;
+			}
+			return current;
+		}
+
+		public static Comic LatestComic(Context context, TextView comicTitleView, ImageView comicImageView)
+		{
+			SetupDatabase(context, out _);
+			RefreshDatabase(context);
+			Comic latest = Xkcd.GetLatestComic();
+
+			LoadComic(context, comicTitleView, comicImageView, latest);
+
+			return latest;
+		}
+	}
+}
