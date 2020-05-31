a8a919a5245c16a1d6b8a35ff8d83e9a82dda690
John Hunter <hunterjw.usa@gmail.com>
2020-05-31 00:04:04 -0500

    Adding a placeholder fragment

--- XkcdBrowser.AndroidApp/MainContentSwitcher.cs
+++ XkcdBrowser.AndroidApp/MainContentSwitcher.cs
@@ -18,31 +18,73 @@ namespace XkcdBrowser.AndroidApp
 			}
 			else if (navBarMenuId == Resource.Id.nav_comic_list)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("Comic List");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_comic_list;
 			}
 			else if (navBarMenuId == Resource.Id.nav_comic_favs)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("Comic Favorite List");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_comic_favs;
 			}
 			else if (navBarMenuId == Resource.Id.nav_whatif_main)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("What If");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_whatif_main;
 			}
 			else if (navBarMenuId == Resource.Id.nav_whatif_list)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("What If List");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_whatif_list;
 			}
 			else if (navBarMenuId == Resource.Id.nav_whatif_favs)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("What If Favorite List");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_whatif_favs;
 			}
 			else if (navBarMenuId == Resource.Id.nav_settings)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("Settings");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_settings;
 			}
 			else if (navBarMenuId == Resource.Id.nav_about)
 			{
+				var placeholderFrag = PlaceholderFragment.NewInstance("About");
 
+				FragmentTransaction tx = mainActivity.SupportFragmentManager.BeginTransaction();
+				tx.Replace(Resource.Id.main_activity_container, placeholderFrag);
+				tx.Commit();
+
+				mainActivity.CurrentContentId = Resource.Id.nav_about;
 			}
 		}
 	}
---
+++ XkcdBrowser.AndroidApp/PlaceholderFragment.cs
@@ -0,0 +1,37 @@
+﻿using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+
+namespace XkcdBrowser.AndroidApp
+{
+	public class PlaceholderFragment : Fragment
+	{
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			View view = inflater.Inflate(Resource.Layout.placeholder_layout, container, false);
+
+			TextView textView = view.FindViewById<TextView>(Resource.Id.placeholderTextView);
+			textView.Text = PlaceholderString;
+
+			return view;
+		}
+
+		public string PlaceholderString => Arguments.GetString("placeholder_string", "Placeholder");
+
+		public static PlaceholderFragment NewInstance(string placeholderString = "")
+		{
+			var bundle = new Bundle();
+			if (!string.IsNullOrEmpty(placeholderString))
+			{
+				bundle.PutString("placeholder_string", placeholderString);
+			}
+			return new PlaceholderFragment { Arguments = bundle };
+		}
+	}
+}
